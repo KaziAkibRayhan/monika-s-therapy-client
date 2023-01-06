3bb5317409970ed73cdde61504a6ed330d28dac1
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2023-01-06 19:20:13 +0600

    load data to server and display data by limit 3 data in home page or route

---
+++ src/Pages/Home/ServiceHome/ServiceCard.js
@@ -0,0 +1,19 @@
+import { Card } from "flowbite-react";
+import React from "react";
+
+const ServiceCard = ({ service }) => {
+  return (
+    <Card imgAlt={service.name} imgSrc={service.img}>
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {service.name}
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        {service.description.length > 100
+          ? service.description.slice(0, 100) + "..."
+          : service.description}
+      </p>
+    </Card>
+  );
+};
+
+export default ServiceCard;
--- src/Pages/Home/ServiceHome/ServiceHome.js
+++ src/Pages/Home/ServiceHome/ServiceHome.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import { Button } from "flowbite-react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ServiceCard from "./ServiceCard";
 
 const ServiceHome = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/homeService")
+      .then((res) => res.json())
+      .then((data) => setServices(data.data));
+  }, []);
+  console.log(services);
+
+  return (
+    <div className="my-10">
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+        {services.map((service) => (
+          <ServiceCard key={service._id} service={service}></ServiceCard>
+        ))}
+      </div>
+      <div className="flex justify-center mt-10">
+        <Link to={"/services"}>
+          <Button color="purple">See All</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceHome;
