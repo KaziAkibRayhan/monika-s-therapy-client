5ee8357308d3cc877090550971d91e9c52e84c52
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2023-01-09 12:01:24 +0600

    envrionment variables setup

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -5,15 +5,24 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDegSkj2ax75L6GOUsQKWpptMfObibM_gA",
-  authDomain: "monikas-therapy-auth.firebaseapp.com",
-  projectId: "monikas-therapy-auth",
-  storageBucket: "monikas-therapy-auth.appspot.com",
-  messagingSenderId: "848990114994",
-  appId: "1:848990114994:web:297be88c22738cccf8a5ab",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
+
+/* 
+apiKey: "AIzaSyDegSkj2ax75L6GOUsQKWpptMfObibM_gA",
+  authDomain: "monikas-therapy-auth.firebaseapp.com",
+  projectId: "monikas-therapy-auth",
+  storageBucket: "monikas-therapy-auth.appspot.com",
+  messagingSenderId: "848990114994",
+  appId: "1:848990114994:web:297be88c22738cccf8a5ab",
+*/
